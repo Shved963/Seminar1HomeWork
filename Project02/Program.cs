288f2171171886e5e9d905a529ae413ab4e9cb15
Andrei Shvetsov <shved963@gmail.com>
2022-11-23 23:45:52 +0300

    Project02 ready

--- Project02/Program.cs
+++ Project02/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+int numC = int.Parse(Console.ReadLine());
+
+
+if (numA > numB & numA > numC )
+{
+    Console.Write("max = ");
+    Console.Write(numA);
+}
+
+if (numB > numC)
+{
+    Console.Write("max = ");
+    Console.Write(numB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(numC);
+}
+
+//Программа выводит максимальное число, даже если два из трех или все числа одинаковые
